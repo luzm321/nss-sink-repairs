ec3cddaa1c40bfbc72dee7b73717da48be67fea1
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-06-15 20:35:45 -0500

    created completion state, added select elements for plumbers to display, created functions to save and fetch completion state, and modified getRequests() method in dataAccess module to return an array of service request objects that are sorted by their completion status via the sort() method; completed explorer chapter for sink repair

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,7 +1,10 @@
+import { render } from "./main.js";
 const mainContainer = document.querySelector("#container");
 
 const applicationState = {
-    requests: []
+    requests: [],
+    plumbers: [],
+    completions: []
 };
 
 // FETCH CALL REQUESTS:
@@ -21,9 +24,50 @@ export const fetchRequests = () => {
         );
 };
 
-//exporting copy of requests state:
+export const fetchPlumbers = () => {
+    return fetch(`${API}/plumbers`)
+        .then(response => response.json())
+        .then(
+            (servicePlumbers) => {
+                // Store the external state in application state
+                applicationState.plumbers = servicePlumbers
+            }
+        );
+};
 
-export const getRequests = () => [...applicationState.requests];
+export const fetchCompletions = () => {
+    return fetch(`${API}/completions`)
+        .then(response => response.json())
+        .then(
+            (serviceCompletions) => {
+                // Store the external state in application state
+                applicationState.completions = serviceCompletions
+            }
+        );
+};
+
+//exporting copy of specified object properties of application state:
+
+export const getRequests = () => {
+    return applicationState.requests.sort((a,b) => {
+        // a is the second thing, b is the first thing in the array, if the second thing is the same as the first thing return 0, if they aren't
+        // the same then is the first thing true, if it is then it returns -1, if it isn't it returns 1.
+        // this sorting allows for all the isCompleted requests to be last and the false ones first.
+        return (a.isCompleted === b.isCompleted) ? 0 : b.isCompleted ? -1 : 1;
+    });
+};
+
+// export const getRequests = () => {
+//     return applicationState.requests.map(request => ({...request}));
+// };
+
+export const getPlumbers = () => {
+    return applicationState.plumbers.map(plumber => ({...plumber}));
+};
+
+export const getCompletions = () => {
+    return applicationState.completions.map(completion => ({...completion}));
+};
 
 //HTTP POST(create something new) Request with Fetch; the POST fetch call will dispatch the stateChanged custom event after the POST operation
 //is completed; every time state changes, you have to generate new HTML representations of the state
@@ -45,6 +89,24 @@ export const sendRequest = (userServiceRequest) => {
         });
 };
 
+export const saveCompletion = (completion) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(completion)
+    };
+
+
+    return fetch(`${API}/completions`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            render();
+        });
+};
+
+
 // Fetching HTTP DELETE Request: when using DELETE method on an HTTP request to the API, you must identify a single resource
 //(object via the id/primary key) as an argument to avoid deleting an entire collection/array:
 
@@ -52,7 +114,8 @@ export const deleteRequest = (id) => {
     return fetch(`${API}/requests/${id}`, { method: "DELETE" })
         .then(
             () => {
-                mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+                // mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+                render();
             }
         );
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,24 @@
-import { fetchRequests } from "./dataAccess.js";
+import { fetchRequests, fetchPlumbers, fetchCompletions, deleteRequest } from "./dataAccess.js";
 import { SinkRepair } from "./SinkRepair.js";
-import { deleteRequest } from "./dataAccess.js";
-
 
 
 const mainContainer = document.querySelector("#container");
-//render function invokes the fetchRequests function which returns the external data from the api database stored in applicationState object, 
+//render function invokes the fetchRequests function which returns the external data from the api database stored in applicationState object,
 //then the data structures can be converted into HTML representations:
-//fetchRequests function receives a promise, which you resolve with .then() which takes a function that enables the returned html representation
-//value of calling the SinkRepair() fxn to be displayed to the DOM after the render() fxn is invoked on line 19:
-const render = () => {
-    fetchRequests().then(
+//fetchRequests function returns a promise, which you resolve with .then() and chain the .then() which takes the fxn fetchPlumbers which returns a
+//promise that you resolve with another .then which takes the fxn fetchCompletions returns another promise that you resolve with yet another .then
+//which takes a function that enables the returned html representation value of calling the SinkRepair() fxn to be displayed to the DOM after the
+// render() fxn is invoked on line 23:
+export const render = () => {
+    fetchRequests()
+    .then(fetchPlumbers)
+    .then(fetchCompletions)
+    .then(
         () => {
             mainContainer.innerHTML = SinkRepair()
         }
     );
 };
-
-mainContainer.addEventListener("click", click => {
-    if (click.target.id.startsWith("request--")) {
-        const [,requestId] = click.target.id.split("--");
-        deleteRequest(parseInt(requestId));
-    };
-});
-
 
 render();
 
